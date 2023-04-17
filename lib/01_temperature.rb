34c0728872c2448314aaa94f6e8fcdf1bd6fd224
Jan Gallani <129413125+Najlagg@users.noreply.github.com>
2023-04-17 23:26:14 +0200

    Add files via upload

---
+++ lib/01_temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(i)
+    return (i - 32) * 5/9
+end
+
+def ctof(i)
+    return (i.to_f * 9/5) + 32
+end
---
+++ lib/02_calculator.rb
@@ -0,0 +1,11 @@
+def add(a, b)
+    return (a + b)
+end 
+
+def subtract(a, b)
+    return (a - b)
+end 
+
+def multiply(a, b)
+    return (a * b)
+end 
